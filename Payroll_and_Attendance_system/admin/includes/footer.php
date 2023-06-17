256e40991f7b6b202848cc8d2889f4891082b788
Nitesh-2002 <niteshagrawalr123@gmail.com>
2023-06-17 13:27:10 +0530

    add all files

---
+++ Payroll_and_Attendance_system/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Made By Nitesh Agrawal</b>
+    </div>
+    <strong>Copyright &copy; Nitesh Attendance and Payroll System </strong>
+</footer>
